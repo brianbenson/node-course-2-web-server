2e5b537accebf9650c0b5fb3a57802c8b2231262
brianbenson <bbenson@helpsoft.com>
2018-08-26 06:14:46 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log')
+        }
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');    
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+// app.get('/', (req, res) => {
+
+//     //res.send('<h1>Hello Express!</h1>');
+//     res.send({
+//         name:  'Andrew',
+//         likes:  [
+//             'Biking',
+//             'Cities'
+//         ]
+//     });
+
+// });
+
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page 22',        
+    });
+});
+
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website'
+    });
+});
+
+
+
+app.get('/bad', (req, res) => {
+
+    //res.send('<h1>Hello Express!</h1>');
+    res.send({
+        errorMessage:  'Unable to handle request'
+    });
+
+});
+
+
+// listen for requests on port 3000
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
